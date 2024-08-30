21a67f0b309e04fe126edfff0ffee17f397b32dc
quanyshev <quanyshev@quanyshevs-MBP.bbrouter>
2024-08-30 21:42:26 +0500

    Clear from jdbc history

--- src/main/java/com/bek/ticketService/dao/TicketDAOImpl.java
+++ src/main/java/com/bek/ticketService/dao/TicketDAOImpl.java
@@ -12,20 +12,7 @@ import java.util.List;
 public class TicketDAOImpl implements TicketDAO {
     Configuration conf = new Configuration().configure().addAnnotatedClass(Ticket.class);
     SessionFactory sf = conf.buildSessionFactory();
-//    private final String url = "jdbc:postgresql://localhost:5432/ticket_services";
-//    private final String username = "postgres";
-//    private final String password = "java";
-//
-//    protected Connection getConnection() {
-//        Connection connection = null;
-//
-//        try {
-//            connection = DriverManager.getConnection(url, username, password);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return connection;
-//    }
+
     @Override
     public void saveTicket(Ticket ticket) {
         try(Session session = sf.openSession()) {
@@ -36,28 +23,6 @@ public class TicketDAOImpl implements TicketDAO {
         }
     }
 
-//    @Override
-//    public void saveTicket(Ticket ticket) {
-//        String saveTicket_sql = "INSERT INTO tickets (user_id, clazz, type, creation_date) VALUES (?, ?, ?, ?)";
-//        try (Connection connection = getConnection();
-//             PreparedStatement preparedStatement = connection.prepareStatement(saveTicket_sql, Statement.RETURN_GENERATED_KEYS)) {
-//            preparedStatement.setInt(1, ticket.getUser_id());
-//            preparedStatement.setObject(2, ticket.getClazz(), java.sql.Types.OTHER);
-//            preparedStatement.setObject(3, ticket.getType(), java.sql.Types.OTHER);
-//            preparedStatement.setDate(4, Date.valueOf(ticket.getStartDate()));
-//            preparedStatement.executeUpdate();
-//            System.out.println("Ticket successfully inserted to database!");
-//
-//            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
-//                if (generatedKeys.next()) {
-//                    ticket.setId(generatedKeys.getInt(1));
-//                    System.out.println("Your ticket id is " + generatedKeys.getInt(1));
-//                }
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
     @Override
     public Ticket getTicketByTicketId(int id) {
         Ticket ticket = null;
@@ -70,33 +35,6 @@ public class TicketDAOImpl implements TicketDAO {
         return ticket;
     }
 
-//    @Override
-//    public Ticket getTicketByTicketId(int id) {
-//        Ticket ticket = null;
-//        String getTicketById_sql = "SELECT * FROM tickets WHERE id=?";
-//        try (Connection connection = getConnection();
-//             PreparedStatement preparedStatement = connection.prepareStatement(getTicketById_sql)) {
-//            preparedStatement.setInt(1, id);
-//
-//            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-//                if (resultSet.next()) {
-//                    int ticketId = resultSet.getInt(1);
-//                    int userId = resultSet.getInt(2);
-//                    String t_class = resultSet.getString(3);
-//                    String t_type = resultSet.getString(4);
-//                    Ticket.TicketClass ticketClass = Ticket.TicketClass.valueOf(t_class);
-//                    Ticket.TicketType ticketType = Ticket.TicketType.valueOf(t_type);
-//                    LocalDate startDate = resultSet.getDate(5).toLocalDate();
-//
-//                    ticket = new Ticket(ticketId, userId, ticketClass, ticketType, startDate);
-//                    System.out.println("Successfully retrieved ticket with id " + id + " from database!");
-//                }
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
 
     @Override
     public ArrayList<Ticket> getTicketByUserId(int id) {
@@ -115,39 +53,7 @@ public class TicketDAOImpl implements TicketDAO {
         }
         return tickets;
     }
-//    @Override
-//    public ArrayList<Ticket> getTicketByUserId(int id) {
-//        ArrayList<Ticket> ticketList = new ArrayList<>();
-//        Ticket ticket = null;
-//        String getTicketByUserId_sql = "SELECT * FROM tickets WHERE user_id=?";
-//        try (Connection connection = getConnection();
-//             PreparedStatement preparedStatement = connection.prepareStatement(getTicketByUserId_sql)) {
-//            preparedStatement.setInt(1, id);
-//
-//            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-//                if (resultSet.next()) {
-//                    int ticketId = resultSet.getInt(1);
-//                    int userId = resultSet.getInt(2);
-//                    String t_class = resultSet.getString(3);
-//                    String t_type = resultSet.getString(4);
-//                    Ticket.TicketClass ticketClass = Ticket.TicketClass.valueOf(t_class);
-//                    Ticket.TicketType ticketType = Ticket.TicketType.valueOf(t_type);
-//                    LocalDate startDate = resultSet.getDate(5).toLocalDate();
-//
-//                    ticket = new Ticket(ticketId, userId, ticketClass, ticketType, startDate);
-//                    ticketList.add(ticket);
-//                }
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        if (!ticketList.isEmpty()) {
-//            System.out.println("Successfully retrieved tickets from database! Amount: " + ticketList.size());
-//        } else {
-//            System.out.println("No tickets found");
-//        }
-//        return null;
-//    }
+
     @Override
     public void deleteTicketbyId(int id) {
         Transaction transaction = null;
@@ -166,18 +72,7 @@ public class TicketDAOImpl implements TicketDAO {
             System.out.println("Transaction failed. Rolled back.");
         }
     }
-//    @Override
-//    public void deleteTicketbyId(int id) {
-//        String deleteUser_sql = "DELETE FROM tickets WHERE id=?";
-//        try (Connection connection = getConnection();
-//             PreparedStatement preparedStatement = connection.prepareStatement(deleteUser_sql)) {
-//            preparedStatement.setInt(1, id);
-//            preparedStatement.executeUpdate();
-//            System.out.println("Successfully deleted ticket with id " + id);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
+
     @Override
     public void updateTicketType(Ticket ticket, Ticket.TicketType ticketType) {
         Transaction transaction = null;
@@ -192,19 +87,6 @@ public class TicketDAOImpl implements TicketDAO {
             transaction.rollback();
         }
     }
-//    @Override
-//    public void updateTicketType(Ticket ticket, Ticket.TicketType ticketType) {
-//        String updateTicketType = "UPDATE tickets SET type=? WHERE id=?";
-//        try (Connection connection = getConnection();
-//             PreparedStatement preparedStatement = connection.prepareStatement(updateTicketType)) {
-//            preparedStatement.setObject(1, ticketType, java.sql.Types.OTHER);
-//            preparedStatement.setInt(2, ticket.getId());
-//            preparedStatement.executeUpdate();
-//            System.out.println("Type of ticket successfully updated!");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
 
 
--- src/main/java/com/bek/ticketService/dao/UserDAOImpl.java
+++ src/main/java/com/bek/ticketService/dao/UserDAOImpl.java
@@ -10,19 +10,6 @@ public class UserDAOImpl implements UserDAO {
     private final Configuration conf = new Configuration().configure().addAnnotatedClass(User.class);
     private final SessionFactory sf = conf.buildSessionFactory();
 
-//    protected Connection getConnection() {
-//        String url = "jdbc:postgresql://localhost:5432/ticket_services";
-//        String username = "postgres";
-//        String password = "java";
-//        Connection connection = null;
-//
-//        try {
-//            connection = DriverManager.getConnection(url, username, password);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return connection;
-//    }
     @Override
     public void saveUser(User user) {
         Transaction transaction = null;
@@ -40,27 +27,6 @@ public class UserDAOImpl implements UserDAO {
         }
     }
 
-//    @Override
-//    public void saveUser(User user) {
-//        String saveUser_sql = "INSERT INTO users (name, creation_date) VALUES (?, ?)";
-//
-//        try (Connection connection = getConnection();
-//             PreparedStatement preparedStatement = connection.prepareStatement(saveUser_sql, Statement.RETURN_GENERATED_KEYS)) {
-//            preparedStatement.setString(1, user.getName());
-//            preparedStatement.setDate(2, Date.valueOf(user.getStartDate()));
-//            preparedStatement.executeUpdate();
-//            System.out.println("User successfully inserted to database!");
-//
-//            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
-//                if (generatedKeys.next()) {
-//                    user.setId(generatedKeys.getInt(1));
-//                }
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
     @Override
     public User getUserById(int id) {
         User user = null;
@@ -73,27 +39,6 @@ public class UserDAOImpl implements UserDAO {
         return user;
     }
 
-//    @Override
-//    public User getUserById(int id) {
-//        User user = null;
-//        String getUser_sql = "SELECT * FROM users WHERE id=?";
-//
-//        try (Connection connection = getConnection();
-//             PreparedStatement preparedStatement = connection.prepareStatement(getUser_sql)) {
-//            preparedStatement.setInt(1, id);
-//
-//            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-//                if(resultSet.next()) {
-//                    user = new User(resultSet.getString(2), resultSet.getInt(1),
-//                            resultSet.getDate(3).toLocalDate());
-//                    System.out.println("User with id " + id + " successfully retrieved from database!");
-//                }
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
     @Override
     public void deleteUserById(int id) {
         Transaction transaction = null;
@@ -113,17 +58,4 @@ public class UserDAOImpl implements UserDAO {
             e.printStackTrace();
         }
     }
-//    @Override
-//    public void deleteUserById(int id) {
-//        String deleteUser_sql = "DELETE FROM users WHERE id=?";
-//
-//        try (Connection connection = getConnection();
-//             PreparedStatement preparedStatement = connection.prepareStatement(deleteUser_sql)) {
-//            preparedStatement.setInt(1, id);
-//            preparedStatement.executeUpdate();
-//            System.out.println("User with id " + id + " successfully deleted from database!");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
